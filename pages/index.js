ba2f3e7146ae559f130ce340e4a8bbc41cfce646
iamajmalsaleem <ajmalsaleem84@gmail.com>
2021-12-23 19:06:28 +0530

    Update Header

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
-import Head from 'next/head'
-import { useMoralis } from 'react-moralis';
-import Login from '../components/Login'
+import Head from "next/head";
+import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
+import Login from "../components/Login";
+import Messages from "../components/Messages";
 
 export default function Home() {
+  const { isAuthenticated } = useMoralis();
 
-  const {isAuthenticated, logout} = useMoralis();
-
-  if(!isAuthenticated) return <Login />
+  if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to dApp</h1>
-      <button className="btn bg-red-200"onClick={logout}>Logout</button>
-     
+      <div className="max-w-screen-2xl mx-auto">
+        <Header />
+        <Messages />
+      </div>
     </div>
-  )
+  );
 }
